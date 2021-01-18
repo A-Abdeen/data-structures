2feeb5692476b0611c45a3c1d5689e85a345b385
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-18 12:49:41 +0300

    basic add function works, need to implment a traverse function in conjunction

--- trees.js
+++ trees.js
@@ -1,37 +1,46 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 class TreeNode {
-  constructor(data) {
-    this.data = data;
+  constructor(name, parent, limit) {
+    this.name = name;
+    this.parent = parent;
     this.children = [];
+    this.limit = limit;
   }
 
-  addChild = (node) => {
-    this.children.push(node);
+  addChild = (name, parent) => {
+    name = prompt(`Who are you, Great child of Badam?  `);
+    parent = prompt(`Where do you come from, ${name}?  `);
+    this.children.push({ name, parent });
   };
 
   removeChild = (node) => {
-    this.children = this.children.filter((child) => child !== node);
+    this.children = this.children.filter((name) => child !== node);
   };
 
   traverse = () => {
-    let nodes = [this];
-    while (nodes.length > 0) {
-      let current = nodes.pop();
-      console.log(current.data);
-      nodes = [...nodes, ...current.children];
-    }
+    if (this.name === parent) console.log(`OKAY`);
+    // nodes = [...nodes, ...current.children];
   };
 }
 
-const root = new TreeNode("Darth Vader");
-const child1 = new TreeNode("Luke Skywalker");
-const child2 = new TreeNode("Leia Organa");
-const child3 = new TreeNode("Kylo Ren");
+// console.table();
 
-root.addChild(child1);
-root.addChild(child2);
-child2.addChild(child3);
-root.removeChild(child2);
+const root = new TreeNode("Great", "Badam", 2);
+// const child1 = new TreeNode("Luke Skywalker");
+// const child2 = new TreeNode("Leia Organa");
+// const child3 = new TreeNode("Kylo Ren");
 
-root.traverse();
+root.addChild();
+// root.addChild();
+// root.traverse();
+// root.addChild();
+// root.addChild();
+// root.addChild();
+
+// root.addChild(child2);
+// child2.addChild(child3);
+// root.removeChild(child2);
+
+// root.traverse();
+console.table(root);
